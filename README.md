384f644b374aa9ea4c7572f40f35eafe6198c43d
Ha1f_Deaf <jaredtspears@gmail.com>
2018-09-03 16:36:31 -0400

    setting basic framing for the app using bootstrap. have created the necessary files for this app

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # WMS_Connections_App
 Intended to be a staff/student connections app that will work only in school buildings for safe communication.
+
+This is a prototype work in progress to better communicate with our staff and students. 
+
+**This is not approved for use at this time.**
---
+++ homePgApp.js
@@ -0,0 +1,24 @@
+
+// want to test firebase app out. Want to have app that interacts
+//with the user,and uses in app messaging with FireBase 
+
+//will need a main function with the ability to add users to an object 
+//or an array to document and leave open the ability to communicate with 
+//each user. 
+    //within the function I will need a loop to check for responses 
+    // buttons for submission (may use a Form Style format on Bootstrap)
+
+//will need vars but not sure where yet putting them here until I find a spot
+//will have a userComment
+const userComment= "";
+
+
+    //creating an on click function tied to the created class1 in the empty div
+$(document).on("click", ".class1", function(){
+        //trying to set up a var for class1 so that I can console log it easier
+    let class1 = $(".class1");
+    console.log("This is class1: " + class1);
+
+    
+
+})
